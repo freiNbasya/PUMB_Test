32a2c6393629083d83b2c8f7e489968f113f64b0
shito.bulo@gmail.com <shito.bulo@gmail.com>
2023-06-12 23:34:01 +0300

    Added a model

---
+++ Models/ContactsModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PUMB_Test.Models
+{
+    public class ContactsModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+    }
+}

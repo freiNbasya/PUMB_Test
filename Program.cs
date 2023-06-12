0a7bc9528a708638233a0d887a3413bc8d383882
shito.bulo@gmail.com <shito.bulo@gmail.com>
2023-06-12 23:47:36 +0300

    Added initial migration

--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=ContactsModels}/{action=Index}");
 
 app.Run();
